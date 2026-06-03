e6db6fb14fb476c98e2ceac420291be6766906f7
surfm4945 <surfm4945@gmail.com>
2026-06-03 14:15:23 +0500

    Delete download_cascade.py

--- download_cascade.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-
-url = "https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml"
-filename = "haarcascade_frontalface_default.xml"
-
-print("Downloading Haar Cascade file...")
-urllib.request.urlretrieve(url, filename)
-print("Download complete! File saved as:", filename)
